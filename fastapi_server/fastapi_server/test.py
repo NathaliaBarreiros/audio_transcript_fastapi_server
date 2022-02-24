fd04120501fe2aa489c1207519388aa4e9278c6c
Nath <nathalia.barreirosf@gmail.com>
2022-02-24 14:44:44 -0500

    cookies & headers

--- fastapi_server/fastapi_server/test.py
+++ fastapi_server/fastapi_server/test.py
@@ -8,9 +8,10 @@ from attr import field
 # Pydantic
 from pydantic import BaseModel
 from pydantic import Field
+from pydantic import EmailStr
 
 # FastAPI
-from fastapi import FastAPI
+from fastapi import FastAPI, Header, Cookie
 from fastapi import status
 from fastapi import Body, Query, Path, Form
 from starlette.types import Message
@@ -29,3 +30,24 @@ def login(
     password: str = Form(...),
 ):
     return LoginOut(username=username)
+
+
+# Cookies and Headers Parameters
+@app.post(path="/contact/", status_code=status.HTTP_200_OK)
+def contact(
+    first_name: str = Form(
+        ...,
+        max_length=20,
+        min_length=1,
+    ),
+    last_name: str = Form(
+        ...,
+        max_length=20,
+        min_length=1,
+    ),
+    email: EmailStr = Form(...),
+    message: str = Form(..., min_length=20),
+    user_agent: Optional[str] = Header(default=None),
+    ads: Optional[str] = Cookie(default=None),
+):
+    return user_agent
